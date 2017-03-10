448622f7de4c1d474e751de80dcd95e93c2f3765
chonsarun <chonsarun2494@gmail.com>
2017-03-10 16:42:41 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -14,7 +14,7 @@ if (!is_null($events['events'])) {
 			// Get text sent
 		
 			$text = $event['message']['text'];
-			if(strpos($text,'scss')
+			if(strpos($text,'scss'))
 			{
 		          $text='scss.pea.co.th';
 			}else{
